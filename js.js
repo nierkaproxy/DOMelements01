0c67800edcdfde49104a15024da5a0be58f06f89
Moksleivis <Moksleivis@kitm.local>
2022-12-02 14:43:45 +0200

    pridetas filtras

--- js.js
+++ js.js
@@ -16,15 +16,16 @@ import allCountries from './data.js';
 
 
 const my_main_div = document.getElementById('main_div');
+const continents_buttons = document.querySelector('header')
+const meniu = ['all'];
 
 
-
-allCountries.map((country, index)=>{
-    console.log(index +1);
+allCountries.map((country, index) => {
+    console.log(index + 1);
     console.log(`Country name: ${country.name.common}`);
     console.log(`Continent: ${country.continents}`);
 
-    
+
     const naujas_divas = document.createElement('div');
     const my_country_name = document.createElement('h2');
     const my_flags = document.createElement('img');
@@ -34,8 +35,10 @@ allCountries.map((country, index)=>{
     const my_currency = document.createElement('h4');
     const lang_title = document.createElement('h3');
     const country_borders_name = document.createElement('h4');
+    const p_divas = document.createElement('div');
 
-    
+
+    p_divas.setAttribute('class', 'p_divas');
     my_country_region.setAttribute("class", "continent");
     my_country_name.innerText = country.name.common;
     my_flags.src = country.flags.png;
@@ -44,37 +47,37 @@ allCountries.map((country, index)=>{
     country_borders_name.innerText = 'Neighbours: '
     // my_flags.setAttribute('src', country.flags.png);
 
-    if(country.continents == "Africa"){
+    if (country.continents == "Africa") {
         my_country_region.setAttribute(
             'style',
             'background-color: yellow;'
         )
-    }else if(country.continents == "Europe"){
+    } else if (country.continents == "Europe") {
         my_country_region.setAttribute(
             'style',
             'background-color: lightgreen;'
         )
-    }else if(country.continents == "South America"){
+    } else if (country.continents == "South America") {
         my_country_region.setAttribute(
             'style',
             'background-color: lightblue;'
         )
-    }else if(country.continents == "North America"){
+    } else if (country.continents == "North America") {
         my_country_region.setAttribute(
             'style',
             'background-color: pink;'
         )
-    }else if(country.continents == "Asia"){
+    } else if (country.continents == "Asia") {
         my_country_region.setAttribute(
             'style',
             'background-color: orange;'
         )
-    }else if(country.continents == "Oceania"){
+    } else if (country.continents == "Oceania") {
         my_country_region.setAttribute(
             'style',
             'background-color: beige;'
         )
-    }else{
+    } else {
         my_country_region.setAttribute(
             'style',
             'background-color: blue;'
@@ -86,57 +89,87 @@ allCountries.map((country, index)=>{
     naujas_divas.appendChild(my_country_name);
     naujas_divas.appendChild(my_country_capital);
     naujas_divas.appendChild(lang_title);
-  
 
-    if(Array.isArray(country.capital)){
+
+    if (Array.isArray(country.capital)) {
         console.log(`Capital: ${country.capital[0]}`);
         my_country_capital.innerText = `Capital: ${country.capital[0]}`;
 
-    }else{
+    } else {
         console.log("Capital data not found")
         my_country_capital.innerText = "Capital data not found"
 
     }
 
-    for(let j in country.languages){
-        console.log(`Language: ${country.languages[j]}`)
+    for (let j in country.languages) {
+        console.log(`Language: ${country.languages[j]} `)
         const li = document.createElement('div');
         li.textContent = `${country.languages[j]}`;
         my_country_lang.appendChild(li);
         naujas_divas.append(my_country_lang);
     }
 
-    for(let k in country.currencies){
+    for (let k in country.currencies) {
         console.log(country.currencies[k].name)
         my_currency.innerText = `Currency: ${country.currencies[k].name}`
         naujas_divas.appendChild(my_currency)
     }
 
-    if(Array.isArray(country.borders)){
+    if (Array.isArray(country.borders)) {
         naujas_divas.appendChild(country_borders_name);
     }
 
-    for(let h in country.borders){
+    for (let h in country.borders) {
         console.log(country.borders[h])
         const country_borders = document.createElement('p');
         country_borders.innerText = `${country.borders[h]}`
-        naujas_divas.append(country_borders)
+        p_divas.append(country_borders)
+        naujas_divas.appendChild(p_divas)
     }
-    
+
+
+    if (!meniu.includes(country.continents[0])) {
+        meniu.push(country.continents[0]);
+    }
 
     my_main_div.appendChild(naujas_divas);
     console.log("********************************");
 })
-// const buttonai = document.querySelector('header')
 
+const card_continents = document.querySelectorAll('h5');
+const my_buttons = document.getElementsByTagName('button');
 
-// const getCountry = () => {
-//     if(buttonai == "Africa"){
-        
-//     }
-// }
+console.log(meniu)
+meniu.forEach(continent => {
+    const myBtn = document.createElement('button');
+    myBtn.innerText = continent;
+    continents_buttons.appendChild(myBtn);
 
-// buttonai.addEventListener('click', getCountry)
+})
 
-// naudoti includes del buttonu, ir ne i hardkodinti
+console.log(my_buttons)
+console.log(card_continents)
+
+for (let one_category of my_buttons) {
+    one_category.addEventListener('click',
+        () => { getCountries(one_category.innerText) })
+}
+
+const getCountries = (continent) => {
+    console.log(continent)
+    for (let i of card_continents) {
+        if (continent === i.innerText || continent === 'all') {
+            i.parentElement.setAttribute(
+                'style',
+                'display: block;'
+            )
+        } else {
+            i.parentElement.setAttribute(
+                'style',
+                'display: none;'
+            )
+        }
+    }
+}
+
 
